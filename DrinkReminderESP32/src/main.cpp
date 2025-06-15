bd015321435dbfc6ecee45cb4958623c9fa8f250
Sanwen <MarsCYJ@outlook.com>
2025-06-15 19:10:06 +0800

    cache version

--- DrinkReminderESP32/src/main.cpp
+++ DrinkReminderESP32/src/main.cpp
@@ -1,4 +1,33 @@
 #include "HX711.h"
+
+#include <WiFi.h>
+#include <time.h>
+
+const char* ssid = "CYJ"; // 替换为你的WiFi名称
+const char* password = "88888888"; // 替换为你的WiFi密码
+const char* ntpServer = "pool.ntp.org";
+const long gmtOffset_sec = 8 * 3600; // 东八区
+const int daylightOffset_sec = 0;
+
+// 封装获取时间戳的函数
+long getTimestamp() {
+    struct tm timeinfo;
+    if (!getLocalTime(&timeinfo)) {
+        Serial.println("Failed to obtain time");
+        return -1;
+    }
+    return mktime(&timeinfo);
+}
+
+
+float drinkingWater(float weight) {
+    // 假设每升水的重量为1000克
+    return weight / 1000.0; // 返回升数
+}
+
+#define CACHE_SIZE 100
+float dataCache[CACHE_SIZE];
+int cacheIndex = 0;
 float Weight = 0;
 
 void setup()
@@ -10,14 +39,41 @@ void setup()
 
 	delay(3000);
 	Get_Maopi();		//获取毛皮
+
+
+
+  // ESP32 WIFI initialization
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+      delay(1000);
+      Serial.println("Connecting to WiFi...");
+  }
+  Serial.println("Connected to WiFi");
+
+  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
+
+
+
 }
 
 void loop()
 {
-	Weight = Get_Weight();	//计算放在传感器上的重物重量
-	Serial.print(float(Weight/1000),3);	//串口显示重量
-	Serial.print(" kg\n");	//显示单位
-	Serial.print("\n");		//显示单位
-	delay(1000);				//延时1s
+	  Weight = Get_Weight();	//计算放在传感器上的重物重量(mg)
+    // 将数据存入缓存
+    dataCache[cacheIndex] = Weight;
+    cacheIndex = (cacheIndex + 1) % CACHE_SIZE;
+
+    Serial.print(float(Weight),3);	//串口显示重量
+	  Serial.print(" ml\n");	//显示单位
+	  Serial.print("\n");		//显示单位
+
+    long timestamp = getTimestamp();
+    if (timestamp != -1) {
+      Serial.print("Timestamp: ");
+      Serial.println(timestamp);
+    }
+
+	
+	delay(500);				//延时10ms
 
 }
